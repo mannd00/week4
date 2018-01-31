2c8a3ef4ac6775a35fc63710fbc07066d09815bd
David <mannd00@my.saintpaul.edu>
2018-01-31 17:48:58 -0600

    started on Lab4

--- Mann_Lab4.sh
+++ Mann_Lab4.sh
@@ -1,4 +1,41 @@
 #!/bin/bash
 
 echo "Welcome to Week 4 Lab Script!"
+echo "We will be working with partitions today. you can read the manual by running man udevadm"
+
+echo "Lets list the partitions on the disk"
+sudo fdisk -l /dev/sda
+cd /tmp
+
+echo "Next lets create a file we can play with partitions in without breaking real hardware"
+dd if=/dev/zero of=./32MB.img bs=1M count=32
+sleep 5
+echo "Now we have a 32MB image file"
+echo " next we will checkout whats in there."
+hexdump ./32MB.img | less
+
+echo "Next we format it with ext4 filesystem"
+mkfs -t ext4 ./32MB.img
+echo "Finally, mount the image so we can put stuff in it"
+sleep 5
+
+mkdir /mnt/tmp
+sudo mount ./32MB.img /mnt/tmp
+mount; df -h
+lsblk
+
+sleep 5
+
+cd /mnt/tmp; ls
+echo "Hello World" > hello.txt
+
+sudo unmount /mnt/tmp
+
+echo "Now we will see if we can find our hello text in the hexdump"
+
+sleep 2
+cd /tmp
+hexdump --canonical ./32MB.img | less
+
+echo "10"
 
